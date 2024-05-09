af3f6e9946df89a9a396a3da448e2239137b06b1
Islam Ahmed <62517820+Hakugami@users.noreply.github.com>
2024-05-09 11:34:22 +0300

    swagger ui

---
+++ TodoService/src/main/java/org/spring/todoservice/configs/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package org.spring.todoservice.configs;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    OpenAPI CustomOpenAPI(){
+        return new OpenAPI()
+                .info(new Info().title("Employee API").version("1.0.0"))
+                .info(new Info().title("Department API").version("1.0.0"));
+    }
+}
